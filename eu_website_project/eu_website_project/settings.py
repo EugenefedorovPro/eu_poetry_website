1a4367644d08ff8d48d9c1980f1b10aaf6c8e199
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-11-16 17:14:06 +0200

    test trial

--- eu_website_project/eu_website_project/settings.py
+++ eu_website_project/eu_website_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-e*(y330r$%udn=x=_myt+@918vqa64#z8ji4_cvla!6=o11dme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
 
 
 # Application definition
--- eu_website_project/eupoetry/tests.py
+++ eu_website_project/eupoetry/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import RawVerses
+from django.utils import timezone
 
-# Create your tests here.
+
+class EupoertyViewsTestCase(TestCase):
+    databases = "__all__"
+
+    # def test_number_rand_words(self):
+    #     number_of_ids = RawVerses.objects.all().values_list("id")
+    #     print("____________", number_of_ids)
+    #     response = self.client.get("/eupoetry/")
+    #     number_of_words = len(response.context["list_of_lists_id_random_word"])
+    #     self.assertEqual(number_of_ids, number_of_words)
+
+    def test_new_verse_access(self):
+        title = "new_verse"
+        date_of_writing = timezone.now()
+        verses = "Text of a new verse"
+        new_verse = RawVerses.objects.create(
+            id=23, title=title, date_of_writing=date_of_writing, verses=verses
+        )
+        response = self.client.get("/eupoetry/")
+        print("____response____________", new_verse.id)
+        self.assertQuerysetEqual([new_verse], [new_verse])
