6809b38370070e46680b8ea6ebc0993118798b06
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-11-21 12:11:12 +0200

    no old verses

--- eu_website_project/eupoetry/views.py
+++ eu_website_project/eupoetry/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import loader
 from .rand_words import RandWord
 from .models import RawVerses, EuPro
-from django.http import HttpResponseRedirect
 import random
 
 
