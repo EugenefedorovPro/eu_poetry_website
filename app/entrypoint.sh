971d8e97c01abb6748dffc3f1c512a47c8078145
eugene <eugene.proskulikov@gmail.com>
2026-01-19 10:43:49 +0200

    added makemigrations to entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate --database=verses
+
+python manage.py makemigrations 
+python manage.py migrate
 python manage.py collectstatic --noinput
 
 gunicorn eu_website_project.wsgi:application --bind 0.0.0.0:8008 --timeout 600
